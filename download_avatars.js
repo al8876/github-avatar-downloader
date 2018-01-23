362fbdbec57403d7acd79dfc73a552cce3d3866d
al8876 <al8876@gmail.com>
2018-01-23 07:14:36 +0000

    download images function added

--- download_avatars.js
+++ download_avatars.js
@@ -1,32 +1,46 @@
 var request = require('request');
-var token = require('./secrets')
+var fs = require('fs');
+var token = require('./secrets');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': 'token ' + token.GITHUB_TOKEN
-    }
-  };
-  // console.log();
+// function getRepoContributors(repoOwner, repoName, cb) {
+//   var options = {
+//     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+//     headers: {
+//       'User-Agent': 'request',
+//       'Authorization': 'token ' + token.GITHUB_TOKEN
+//     }
+//   };
+//   // console.log();
 
 
-  request(options, function(err, res, body) {
-    var parse = JSON.parse(body);
-    cb(err, parse);
-    parse.forEach(function() {
-      for (var i = 0; i < 10; i++) {
-        var avatar = parse[i];
-        console.log("Avatar URL: ", avatar.avatar_url);
-      }
-    });
-  });
+//   request(options, function(err, res, body) {
+//     var parse = JSON.parse(body);
+//     cb(err, parse);
+//     parse.forEach(function() {
+//       for (var i = 0; i < 10; i++) {
+//         var avatar = parse[i];
+//         console.log("Avatar URL: ", avatar.avatar_url);
+//       }
+//     });
+//   });
+// }
+
+
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   console.log("Result:", result);
+// });
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function(err) {
+      throw err;
+    })
+    .on('response', function(response) {
+      console.log('Response Status Code: ', response.statusCode);
+      console.log('Response Status Message: ', response.statusMessage);
+      console.log('Response Headers: ', response.headers['content-type']);
+    })
+    .pipe(fs.createWriteStream(filePath));
 }
-
-
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
